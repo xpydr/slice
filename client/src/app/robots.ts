39e5908ba044431ba3c625ea9a803596bb272240
xpydr <purirohan62@gmail.com>
2026-01-15 17:01:41 -0500

    feat(seo): add robots.ts

---
+++ client/src/app/robots.ts
@@ -0,0 +1,12 @@
+import { MetadataRoute } from 'next'
+
+export default function robots(): MetadataRoute.Robots {
+  return {
+    rules: {
+      userAgent: '*',
+      allow: '/',
+      disallow: ['/dashboard/', '/api/'],
+    },
+    sitemap: 'https://www.sliceapi.com/sitemap.xml',
+  }
+}
